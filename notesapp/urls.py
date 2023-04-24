5de2e78de84e015d8c7b06ffcd6bcdb633c15695
Supriya <surkarsupriya97@gmail.com>
2023-04-24 18:33:52 +0000

    updated file

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -1,4 +1,4 @@
-"""notesapp URL Configuration
+"""NotesApp URL Configuration with all URL-Patterns
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
